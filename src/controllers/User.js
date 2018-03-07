3532596044cd4360c9ae0aead333e7206ad3dccf
Alex Symbol <simvol710@gmail.com>
2018-03-07 13:30:29 +0200

    create CRUD model

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -9,7 +9,6 @@ export default class TestCtrl extends BaseCtrl {
     async getList(ctx) {
         try {
             const items = await User.find().lean();
-
             ctx.ok(items);
         } catch (err) {
             ctx.throw(HttpStatus.BAD_REQUEST, err.message);
@@ -18,21 +17,45 @@ export default class TestCtrl extends BaseCtrl {
 
     @post('')
     async createItem(ctx) {
-        // empty
+        try {
+            const item = await User.create(ctx.request.body);
+            ctx.ok(item);
+        }
+        catch (err) {
+            ctx.throw(HttpStatus.BAD_REQUEST, err.message);
+        }
     }
 
     @get('/:_id')
     async getItemById(ctx) {
-        // empty
+        try {
+            const item = await User.findById(ctx.params._id);
+            ctx.ok(item);
+        }
+        catch (err) {
+            ctx.throw(HttpStatus.BAD_REQUEST, err.message);
+        }
     }
 
     @put('/:_id')
     async updateItem(ctx) {
-        // empty
+        try {
+            await User.findByIdAndUpdate(ctx.params._id, {$set: ctx.request.body});
+            ctx.ok('UPDATE SUCCSESS');
+        }
+        catch (err) {
+            ctx.throw(HttpStatus.BAD_REQUEST, err.message);
+        }
     }
 
     @del('/:_id')
     async deleteItem(ctx) {
-        // empty
+        try {
+            await User.findByIdAndRemove(ctx.params._id);
+            ctx.ok('DELETE SUCCESS')
+        }
+        catch (err) {
+            ctx.throw(HttpStatus.BAD_REQUEST, err.message);
+        }
     }
 }
