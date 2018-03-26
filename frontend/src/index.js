ea225689d0f3b7f013e1006db77ee603fbe70558
Alex Symbol <simvol710@gmail.com>
2018-03-26 18:34:04 +0300

    test heroku4

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,5 @@ import './promise-polyfill'
 import 'element-ui/lib/theme-chalk/index.css'
 import './index.scss'
 import { app } from './app'
-
+require('offline-plugin/runtime').install();
 app.$mount('#app');
