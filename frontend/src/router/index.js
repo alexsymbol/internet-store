c1ff1ace7cc08d77398e5d7b6679e01db2452be3
Alex Symbol <simvol710@gmail.com>
2018-03-29 02:28:05 +0300

    test heroku 49

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import LogIn from './views/LogIn.vue';
-import SignUp from './views/SignUp.vue';
-import Profile from '../views/Profile.vue';
-import Cart from '../views/Cart.vue';
+import LogIn from '../views/LogIn';
+import SignUp from '../views/SignUp';
+import Profile from '../views/Profile';
+import Cart from '../views/Cart';
 
 Vue.use(Router);
 
